b1e877934cd0939a54492da6d91eca95392ca710
清水一貴 <s.kazutaka55555@gmail.com>
2018-02-05 19:21:49 +0900

    サービス名を変えた。tokenが切れてる場合の処理も書いた

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,7 +1,6 @@
 <?php
 
-// ログインできるようにする（sessionにaccessTokenを入れる？）
-// エンティティの更新
+//　リファクタ
 
 
 namespace AppBundle\Controller;
@@ -66,6 +65,10 @@ class DefaultController extends BaseController
         if ($accessToken) {
             $client = $this->createGoogleClient();
             $client->setAccessToken($accessToken);
+            // tokenはあっても、期限が切れている場合
+            if ($client->isAccessTokenExpired()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            }
             if ($client->getAccessToken()) {
               $token_data = $client->verifyIdToken();
             }
@@ -253,148 +256,6 @@ class DefaultController extends BaseController
         return $freeTimesText;
     }
 
-
-
-
-
-    // /**
-    //  * @Route("/result", name="yotei-kun-get-freetime")
-    //  * @Method("POST")
-    //  */
-    // public function getFreeTimeAction(Request $request)
-    // {            
-    //     $scheduleSettingEntity = new ScheduleSetting();
-    //     $form = $this->createScheduleSettingForm($scheduleSettingEntity);
-    //     $form->handleRequest($request);
-    //     if (!$form->isValid()) {
-    //         dump($form->getErrors(true));
-    //         exit;
-    //         return $this->redirect($this->generateUrl('yotei-kun'));
-    //     }
-
-    //     $client = $this->getClient();
-    //     $calendarService = new \Google_Service_Calendar($client);
-    //     $calendarList = $calendarService->calendarList->listCalendarList();
-    //     $calendarArray = [];
-
-    //     // Put together our calendar array
-    //     while(true) {
-    //       foreach ($calendarList->getItems() as $calendarListEntry) {
-    //           $calendarArray[] = ['id' => $calendarListEntry->id ];
-    //       }
-    //       $pageToken = $calendarList->getNextPageToken();
-    //       if ($pageToken) {
-    //           $optParams = array('pageToken' => $pageToken);
-    //           $calendarList = $calendarService->calendarList->listCalendarList($optParams);
-    //       } else {
-    //           break;
-    //       }
-    //     } 
-
-    //     $freebusy = new \Google_Service_Calendar_FreeBusyRequest();
-    //     $freebusy->setTimeMin($scheduleSettingEntity->getDayFrom()->format("c"));
-    //     $freebusy->setTimeMax($scheduleSettingEntity->getDayTo()->format("c"));
-    //     $freebusy->setTimeZone('Asia/Tokyo');
-    //     $freebusy->setItems($calendarArray);
-    //     $results = $calendarService->freebusy->query($freebusy);
-
-    //     // $freeTimes = [];
-    //     $beforeBusyEndTime;
-    //     $i = 0;
-    //     $busyArray = $results->getCalendars()["s.kazutaka55555@gmail.com"]->getBusy();
-    //     $isTodayFirstBusy = true;
-    //     $minimumUnit = $scheduleSettingEntity->getMinimumUnit();
-    //     $interval = $scheduleSettingEntity->getInterval();
-    //     // dump($scheduleSettingEntity->getTimeFrom());
-    //     // exit;
-        
-    //     $dateInterval = new \DateInterval('P1D');
-    //     $daterange = new \DatePeriod($scheduleSettingEntity->getDayFrom(), $dateInterval ,$scheduleSettingEntity->getDayTo());
-    //     foreach ($daterange as $date) {
-    //         $this->freeTimes[$date->format("Y年m月d日")] = null;
-    //     }
-
-    //     foreach ($busyArray as $busy) {
-    //         // 予定の開始関連
-    //         $startDateTime = new \DateTime($busy->getStart());
-    //         $startYear = $startDateTime->format("Y");
-    //         $startMonth = $startDateTime->format("m");
-    //         $startDay = $startDateTime->format("d");
-    //         // 予定の終了関連
-    //         $endDateTime = new \DateTime($busy->getEnd());
-
-    //         $timeFrom = $scheduleSettingEntity->getTimeFrom()->setDate($startYear, $startMonth, $startDay);
-    //         $timeTo = $scheduleSettingEntity->getTimeTo()->setDate($startYear, $startMonth, $startDay);
-
-    //         if ($timeFrom < $startDateTime) {
-    //             if($isTodayFirstBusy){// その日最初の予定だった場合
-    //                 dump(0);
-    //                 $this->addFreeTime($startDateTime, $timeFrom, $startDateTime, $minimumUnit, $interval, 1);
-    //             }else{
-    //                 // その予定のstartTimeがtimeToよりも後ならtimeToを使うべき
-    //                 if ($startDateTime > $timeTo) {
-    //                     dump(1);
-    //                     $this->addFreeTime($startDateTime, $beforeBusyEndTime, $timeTo, $minimumUnit, $interval);
-    //                 }else{
-    //                     dump(2);
-    //                     $this->addFreeTime($startDateTime, $beforeBusyEndTime, $startDateTime, $minimumUnit, $interval);
-    //                 }
-    //             }
-    //             $beforeBusyEndTime = $endDateTime;
-    //         }elseif($timeFrom > $endDateTime){
-    //             $beforeBusyEndTime = $timeFrom;
-    //         }else{
-    //             $beforeBusyEndTime = $endDateTime;
-    //         }
-
-    //         if (array_key_exists($i+1, $busyArray)) {
-    //             $nextBusyStartDateTime = new \DateTime($busyArray[$i+1]->getStart());
-    //             if ($startDateTime->format("Ymd") !== $nextBusyStartDateTime->format("Ymd")) {//その日最後の予定だった場合
-    //                 // 予定のスタート時間 < 範囲の終わり時間かつ予定の終わり
-    //                 if ($endDateTime < $timeTo) {
-    //                     if ($isTodayFirstBusy) {
-    //                         dump(3);
-    //                         $this->addFreeTime($startDateTime, $beforeBusyEndTime, $timeTo, $minimumUnit, $interval, 2);
-    //                     }else{
-    //                         dump(4);
-    //                         $this->addFreeTime($startDateTime, $endDateTime, $timeTo, $minimumUnit, $interval, 2);
-    //                     }
-    //                 }
-    //                 $i++;
-    //                 $isTodayFirstBusy = true;
-    //                 continue;
-    //             }
-    //         }else{
-    //             if ($endDateTime < $timeTo) {
-    //                 // ここでもbeforebusyEndtimeを使う必要のあるパターン
-    //                 if ($isTodayFirstBusy) {
-    //                     dump(5);
-    //                     $this->addFreeTime($startDateTime, $beforeBusyEndTime, $timeTo, $minimumUnit, $interval, 2);
-    //                 }else{
-    //                     dump(6);
-    //                     $this->addFreeTime($startDateTime, $endDateTime, $timeTo, $minimumUnit, $interval, 2);
-    //                 }
-    //             }
-    //         }
-    //         $i++;
-    //         $isTodayFirstBusy = false;
-    //     }
-    //     ksort($this->freeTimes);
-
-    //     $freeTimesText = [];
-    //     foreach ($this->freeTimes as $day => $times) {
-    //         $str = "{$day}:";
-    //         foreach ((array)$times as $time) {
-    //             $str .= "{$time}, ";
-    //         }
-    //         $freeTimesText[] = rtrim($str, ", ");
-    //     }
-
-    //     return $this->render('default/result.html.twig', array(
-    //         "freeTimesText" => $freeTimesText,
-    //     ));
-    // }
-
     private function addFreeTime($thisDay, $timeFrom, $timeTo, $minimumUnit, $intervals, $type=0){
         $from = clone $timeFrom;
         $to = clone $timeTo;
@@ -409,113 +270,5 @@ class DefaultController extends BaseController
             }
             $this->freeTimes[$thisDay->format("Y年m月d日")][] = "{$from->format("H:i")}~{$to->format("H:i")}";
         }
-    }
-
-    // private function getClient(){
-    //     $client = new \Google_Client();
-    //     $client->setApplicationName("yotei-kun");
-    //     $client->setScopes(implode(' ', array(\Google_Service_Calendar::CALENDAR_READONLY)));
-    //     $client->setAuthConfig($this->get('kernel')->getRootDir()."/client_secret.json");
-    //     $client->setAccessType('offline');
-    //     $client->setApprovalPrompt('force');
-    //     // Load previously authorized credentials from a file.
-    //     // DB上にあるcredentialを探す
-
-
-    //     $path = '~/.credentials/calendar-php-quickstart.json';
-    //     $homeDirectory = getenv('HOME');
-    //     if (empty($homeDirectory)) {
-    //         $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
-    //     }
-    //     $credentialsPath = str_replace('~', realpath($homeDirectory), $path);
-    //     if (file_exists($credentialsPath)) {
-    //         $accessToken = json_decode(file_get_contents($credentialsPath), true);
-    //     } else {
-    //         // credentialsを作る
-    //         // authCodeを取得し、それをDBに保存
-            
-
-
-
-
-
-
-    //         // Request authorization from the user.
-    //         $authUrl = $client->createAuthUrl();
-    //         header("Location: {$authUrl}");
-    //         exit;
-    //         // 標準入力からinputを待つコードになっている
-    //         $authCode = trim(fgets(STDIN));
-
-    //         // Exchange authorization code for an access token.
-    //         $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-    //         // Store the credentials to disk.
-    //         if(!file_exists(dirname($credentialsPath))) {
-    //           mkdir(dirname($credentialsPath), 0700, true);
-    //         }
-    //         file_put_contents($credentialsPath, json_encode($accessToken));
-    //         printf("Credentials saved to %s\n", $credentialsPath);
-    //     }
-    //     $client->setAccessToken($accessToken);
-
-    //     // Refresh the token if it's expired.
-    //     if ($client->isAccessTokenExpired()) {
-    //         $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-    //         file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
-    //     }
-    //     return $client;
-    // }
-
-
-
-
-    private function getClient(){
-        $client = new \Google_Client();
-        $client->setApplicationName("yotei-kun");
-        $client->setScopes(implode(' ', array(\Google_Service_Calendar::CALENDAR_READONLY)));
-        $client->setAuthConfig($this->get('kernel')->getRootDir()."/client_secret.json");
-        $client->setAccessType('offline');
-        $client->setApprovalPrompt('force');
-        // Load previously authorized credentials from a file.
-        // DB上にあるcredentialを探す
-        // ログインした時に帰ってくる値を使って、
-
-
-        $path = '~/.credentials/calendar-php-quickstart.json';
-        $homeDirectory = getenv('HOME');
-        if (empty($homeDirectory)) {
-            $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
-        }
-        $credentialsPath = str_replace('~', realpath($homeDirectory), $path);
-        if (file_exists($credentialsPath)) {
-            $accessToken = json_decode(file_get_contents($credentialsPath), true);
-        } else {
-            // credentialsを作る
-            // authCodeを取得し、それをDBに保存
-            // Request authorization from the user.
-            $authUrl = $client->createAuthUrl();
-            header("Location: {$authUrl}");
-            exit;
-            // 標準入力からinputを待つコードになっている
-            $authCode = trim(fgets(STDIN));
-
-            // Exchange authorization code for an access token.
-            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-            // Store the credentials to disk.
-            if(!file_exists(dirname($credentialsPath))) {
-              mkdir(dirname($credentialsPath), 0700, true);
-            }
-            file_put_contents($credentialsPath, json_encode($accessToken));
-            printf("Credentials saved to %s\n", $credentialsPath);
-        }
-        $client->setAccessToken($accessToken);
-
-        // Refresh the token if it's expired.
-        if ($client->isAccessTokenExpired()) {
-            // dump($client->getRefreshToken());
-            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-            file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
-        }
-        return $client;
     }
 }
